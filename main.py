25485acf763b876a081525490ddbb10a1ce4945c
Daniel McCave <mccavedaniel@icloud.com>
2026-04-03 00:46:29 -0700

    Implement budget management application with transactions

---
+++ main.py
@@ -0,0 +1,121 @@
+class Transaction:
+    def __init__(self, date, description, category, transaction_type, amount):
+        self.amount = amount
+        self.category = category
+        self.date = date
+        self.type = transaction_type
+        self.description = description
+
+    def to_dict(self):
+        return {
+            "transaction_type": self.type,
+            "category": self.category,
+            "description": self.description,
+            "date": self.date,
+            "amount": self.amount
+        }
+
+
+class Budget:
+    def __init__(self):
+        self.transactions = []
+
+    def add_transaction(self, transaction):
+        self.transactions.append(transaction)
+
+    def get_expenses(self):
+        return [t for t in self.transactions if t.type == "expense"]
+    
+    def get_income(self):
+        return [t for t in self.transactions if t.type == "income"]
+    
+
+class Report:
+    def __init__(self, budget):
+        self.budget = budget
+
+    def total_expenses(self):
+        return sum(t.amount for t in self.budget.get_expenses())
+
+    def total_income(self):
+        return sum(t.amount for t in self.budget.get_income())
+    
+    def balance(self):
+        return self.total_income() - self.total_expenses()
+    
+import json
+
+class Storage:
+    def save(self, transactions):
+        data = [t.to_dict() for t in transactions]
+        with open("data.json", "w") as f:
+            json.dump(data, f, indent=4)
+
+    def load(self):
+        try:
+            with open("data.json", "r") as f:
+                data = json.load(f)
+                return [Transaction(**t)for t in data]
+        except FileNotFoundError:
+            return []
+        
+class App:
+    def __init__(self):
+        self.budget = Budget()
+        self.report = Report(self.budget)
+        self.storage = Storage()
+        loaded_transactions = self.storage.load()
+        self.budget.transactions = loaded_transactions
+
+    def create_transaction(self, transaction_type):
+        self.transaction_type = transaction_type
+        if transaction_type == "income":
+            print("\nYou selected to Add Income!\n")
+            date = input("Enter the date (YYYY-MM-DD): ")
+            description = input("Enter A Short Description (Job, W2, 1099): ")
+            category = input("Enter A Category (Deposit, Check, Side Hustle): ")
+            amount = float(input("Enter The Amount: "))
+
+            t = Transaction(date, description, category, "income", amount)
+            self.budget.add_transaction(t)
+            self.storage.save(self.budget.transactions)
+
+        else:
+            print("\nYou selected To Add A Expense!\n")
+            date = input("Enter The Date (YYYY-MM-DD): ")
+            description = input("Enter A Short Desctription (fast-food, Movie, Clothes): ")
+            category = input("Enter A Category (Bill, Lunch, Groceries): ")
+            amount = float(input("Enter The Amount: "))
+
+            t = Transaction(date, description, category, "expense", amount)
+            self.budget.add_transaction(t)
+            self.storage.save(self.budget.transactions)
+
+
+    def run(self):
+        while True:
+            choice = int(input("\n1. Add Income\n2. Add expense\n3. View Balance\n4. View Transactions\n5. Exit Program\n"))
+
+            if choice < 1 or choice > 5:
+                print("Invalid input. Please select a option from the menu.")
+                continue
+            elif choice == 1:
+                self.create_transaction("income")
+
+            elif choice == 2:
+                self.create_transaction("expense")
+            
+            elif choice == 3:
+                print("Balance:", self.report.balance())
+
+            elif choice == 4:
+                print("You selected to View your Transactions.")
+                for t in self.budget.transactions:
+                    print(f"\n{t.date} | {t.description} | {t.category} | {t.type} | {t.amount}\n")
+
+            elif choice == 5:
+                print("You selected to End the program.\n All of your transactions have been saved!" \
+                "Have a Great Day")
+                break
+
+App().run()
