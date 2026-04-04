90eadad361b905134759e08bd2ba1988f5fd7f41
Daniel McCave <mccavedaniel@icloud.com>
2026-04-03 18:12:55 -0700

    Refactor Transaction class attributes and to_dict method

--- transaction.py
+++ transaction.py
@@ -1,16 +1,16 @@
 class Transaction:
     def __init__(self, date, description, category, transaction_type, amount):
-        self.amount = amount
-        self.category = category
         self.date = date
-        self.type = transaction_type
         self.description = description
+        self.category = category
+        self.transaction_type = transaction_type
+        self.amount = amount
 
     def to_dict(self):
         return {
-            "transaction_type": self.type,
-            "category": self.category,
-            "description": self.description,
             "date": self.date,
+            "description": self.description,
+            "category": self.category,
+            "transaction_type": self.transaction_type,
             "amount": self.amount
         }
